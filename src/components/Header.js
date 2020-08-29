6338eab5b78bf734d69a13c3a627429d8a9b3d5b
JJ <jguaragno@mansueto.com>
2020-08-29 18:46:51 -0400

    original updates

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>BELIEVE IT OR NOT GEORGE ISN'T AT HOME...</header>
+  );
+}
+
+export default Header;
---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Header from '../components/Header';
+
+const AboutPage = () => {
+  return (
+    <Fragment>
+      <Header />
+      <div>This is the new homepage of the maple syrup champione.</div>
+    </Fragment>
+  );
+}
+
+export default AboutPage;
